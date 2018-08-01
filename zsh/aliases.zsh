e506bcefe4fd9e5cdfc2efd09338f6fe30e05c86
Isaac Kuek <isaackuek@users.noreply.github.com>
2018-08-01 15:21:51 -0700

    Amended brewfile

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,10 @@
 alias reload!='. ~/.zshrc'
 
+SERVER='https://rqa.concur.com'
+USERNAME='isaac.kuek'
+
 alias cls='clear' # Good 'ol Clear Screen command
+alias rqa='openconnect ${SERVER}'
+
+## Aliases for Concur Projects
+alias startnui='npm run pack:vendor:dev; npm run dev'
